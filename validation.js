411d06760e148ccf0ae308c953b45cb57c9f5f14
Isaac Kalumba <ikalumba221@gmail.com>
2024-05-17 00:50:58 +0300

    add new validation files

---
+++ validation.js
@@ -0,0 +1,19 @@
+let fullName = document.getElementById("fullname");
+let fullNameError = document.getElementById("fullnameError");
+
+const validation = (event) => {
+  let error = 0;
+  if (fullName.value == "") {
+    fullName.style.border = "1px solid red";
+    fullNameError.textContent = "Please enter Fullname";
+    fullNameError.style = "color:red";
+    error++;
+  } else {
+    fullName.style.border = "1px solid green";
+    fullNameError.style = "";
+  }
+
+  if (error > 0){
+    event.preventDefault()
+  }
+};
